b81c902d03fd14b15ebc4077523c830c7334346e
LOGANATHANTECH <vloganathantech@gmail.com>
2025-08-25 18:06:03 +0530

    start learning java script

---
+++ Week Two/learnJs.js
@@ -0,0 +1,23 @@
+// alert(value)
+// var value;
+
+// if(value){
+//     console.log(value);
+    
+//     alert(true);
+// }else{
+//     console.log(value);
+//     alert(false);
+// }
+
+// let names= [1,2,3,4];
+// let names2= {myname:"loganathan",myname2:"logu"};
+// console.log(names2);
+// function newFunction() {
+//     let a = Number(prompt("enter a number :"));
+//     alert(typeof (a));
+//     let b = String(prompt("enter a String : "));
+//     alert(typeof (b));
+// }
+
+
