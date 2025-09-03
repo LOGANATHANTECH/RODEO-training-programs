b44a54c4fdfe697384075499cd8afb58c88458bc
LOGANATHANTECH <vloganathantech@gmail.com>
2025-09-03 11:58:54 +0530

    ToDo app has been created and started

---
+++ Week Three/ToDoApp/src/board.jsx
@@ -0,0 +1,15 @@
+import Inbox from "./components/inbox"
+import './board.css'
+
+function Board() {
+
+
+  return (
+    <div className="board">
+      <Inbox />
+
+    </div>
+  )
+}
+
+export default Board;
---
+++ Week Three/ToDoApp/src/components/inbox.jsx
@@ -0,0 +1,19 @@
+import "./css/inbox.css"
+function Inbox() {
+    return(
+        <div className="index">
+            <div className="indexTop">
+            <h2>i am index top</h2>
+            </div>
+            <div className="indexMid">
+            <h2>i am index mid</h2>
+
+            </div>
+            <div className="indexBot">
+            <h2>i am index bot</h2>
+
+            </div>
+        </div>
+    );
+}
+export default Inbox;
